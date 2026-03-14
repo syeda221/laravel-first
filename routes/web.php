fc3fb6d02869192723cfd8cb62eaad2ee290b57a
qunoot zehra <qunootzehra21@gmail.com>
2026-03-14 08:47:42 +0500

    u

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ use App\Http\Controllers\UserController;
 //     return view('welcome');
 // });
 Route::get('/add-student',[UserController::class,'add_student']);
-Route::post('/save-student',[UserController::class,'save_student'])->name('adding');
+// Route::post('/save-student',[UserController::class,'save_student'])->name('adding');
 Route::get('/show-student',[UserController::class,'show_student']);
