c6c64d6ddd321773127450d55e6a815f59b7997a
qunoot zehra <qunootzehra21@gmail.com>
2026-04-09 10:34:28 +0500

    added comments

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,10 +7,11 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    //add function 
     public function add_student(){
         return view('Student.add_student');
     }
+    //save function
   public function save_student(Request $request){
     Student::create([
         'name' => $request->name,
@@ -19,23 +20,26 @@ class UserController extends Controller
 
     return redirect("/show-student");
 }
-
+//fetch function
 public function showstudent()
 {
     $students = Student::all();
     return view('Student.show_student', compact('students'));
 }
 
-public function edit_student($id)
-{
-    $student_details = Student::find($id);
-    return view('Student.edit_student', compact('student_details'));
-}
+//delete fucntion
 public function delete_student($id){
     Student::destroy($id);
     return redirect('/show-student');
 
 }
+//edit function
+public function edit_student($id)
+{
+    $student_details = Student::find($id);
+    return view('Student.edit_student', compact('student_details'));
+}
+//update function
 public function update_student(Request $request, $id){
     $student = Student::find($id);
    $newdata=  Student::where('id',$id)->update([
